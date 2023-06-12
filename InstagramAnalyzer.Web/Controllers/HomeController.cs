94af0d1edbe3cde9041675b6696b2cb5875e3a67
Volkan BİÇEN <vbicen1@gmail.com>
2023-06-12 14:44:10 +0300

    add .Web app Instagram Login Page

---
+++ InstagramAnalyzer.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using InstagramAnalyzer.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InstagramAnalyzer.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            return View();
+        }
+      
+    }
+}
---
+++ InstagramAnalyzer.Web/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InstagramAnalyzer.Web.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
